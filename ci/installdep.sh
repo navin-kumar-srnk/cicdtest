3be201de4617e27e21cfb7b927cfa0e9970a8fc6
Naveen.sk <naveensk@V-labs.ai>
2022-11-11 00:29:28 +0530

    run commands added

--- ci/installdep.sh
+++ ci/installdep.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 # Install dependencies and run unit tests
 
-set -e
-cd ${application_repo}/web
+
+cd cicd
 echo "********** Installing Node.js packages using \"npm ci\" command **********"
-npm i
-echo "********** Running test **********"
+npm Install
+
