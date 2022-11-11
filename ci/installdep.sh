d8cfc6157cf67409a2493e162d1aa85a8d36e78c
Naveen.sk <naveensk@V-labs.ai>
2022-11-12 00:29:28 +0530

    jhj

--- ci/installdep.sh
+++ ci/installdep.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 # Install dependencies and run unit tests
-set -e   
 
-cd ..
-
+set -e
 cd cicd
-
 echo "********** Installing Node.js packages using \"npm ci\" command **********"
-
 npm Install
 
