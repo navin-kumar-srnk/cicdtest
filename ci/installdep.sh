abec7ef76efb91abdae473fd45b71ba9ac6345d9
Naveen.sk <naveensk@V-labs.ai>
2022-11-12 00:25:06 +0530

    ppp

--- ci/installdep.sh
+++ ci/installdep.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # Install dependencies and run unit tests
+echo "**********in installdep.sh**********"
 
 set -e   
+
 cd ..
+
 cd cicd
+
 echo "********** Installing Node.js packages using \"npm ci\" command **********"
+
 npm Install
 
